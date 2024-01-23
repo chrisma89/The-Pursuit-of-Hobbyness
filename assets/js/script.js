d15989cee910331dd058dd472244055ea73d2d5d
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-23 08:15:52 +0000

    forgot to save video-wrapper change in script.js, working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,7 +212,7 @@ function fetchData(dataCategory) {
             videoSection.append(videoMain);
 
             for (let i = 0; i < videoItems.length; i++) {
-              let videoDIv = $("<div>").addClass("col-md-6 col-xl-4 videos");
+              let videoDIv = $("<div>").addClass("col-md-6 col-xl-4 video-wrapper");
               let videoID = data.items[i].id.videoId;
               console.log(videoID);
               let videoTitle = data.items[i].snippet.title.trim().toLowerCase();
